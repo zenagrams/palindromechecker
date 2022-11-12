5813f06cc6b007258fb65d0501d25f61757dbfdb
Zenagrams <zadamenwu@gmail.com>
2022-11-12 08:14:25 -0800

    Create a regex to check for nonalphanumeric characters and spaces, then replace with nothing so that we are left with one string ready for palindrome function logic.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-let str ="";
-let lowerStr ="";
+let str ="";//declare variable
+let lowerStr ="";//declare variable
 
-function palindrome(str){
-   lowerStr = str.toLowerCase();
-   console.log(lowerStr); 
+function palindrome(str){ //create function that takes any string
+   lowerStr = str.toLowerCase();//convert string to lowercase and store in new variable
+   let regex = /[^A-Za-z]/g; //create regex that will search for all nonalphanumeric char, g flag means to keep searching after finds a match
+   let cleanStr = lowerStr.replace(regex,"") //use replace method to find regex matches and replace with nothing
+
+   console.log(lowerStr);//check to see if str is converted to lowercase
+   console.log(cleanStr); //check to see if str has no alphanumeric char or spaces
 
 }
 
